1453039f1b72a344d818fb918fc984782e844a37
VRodrigo <victorrodrigo64c@gmail.com>
2017-02-10 10:43:35 +0100

    add sysevent

---
+++ aportaciones/python/sysevents/folderEvents.py
@@ -0,0 +1,19 @@
+import pyinotify
+
+class MyEventHandeler(pyinotify.ProcessEvent):
+    def process_IN_ACCESS(self, event):
+        print "ACCESS event: ", event.pathname
+    def process_IN_CREATE(self, event):
+        print "CREATE event: ", event.pathname
+
+def main():
+    wm = pyinotify.WatchManager()
+    wm.add_watch('/home/dam/prueba', pyinotify.ALL_EVENTS, rec=True)
+
+    eh = MyEventHandeler()
+
+    notifier = pyinotify.Notifier(wm, eh)
+    notifier.loop()
+
+if __name__ == '__main__':
+    main()
